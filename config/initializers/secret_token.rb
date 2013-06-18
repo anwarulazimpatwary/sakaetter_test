eb52741819b5d50c70bb36eaee58830444758e3a
Anwarul Azim Patwary <azim@Robin.local>
2013-06-18 11:16:53 +0900

    summer_test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummerTest05::Application.config.secret_token = '26134824ed9979c07468b1309b3a2e68441dbad88279671831f9f3f5a73e9fdbfaa46b10a8a01fcb1c4b379ec19d7dee4a2d33612891f38049ffd0a6bb0a0703'
